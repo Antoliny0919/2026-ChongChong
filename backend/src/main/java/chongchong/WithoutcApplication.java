12694559a1ab5bbecf1042659d000d915b538388
wontop02 <wjy2002kr@gmail.com>
2026-08-05 14:22:32 +0900

    chore: Spring Boot 프로젝트 세팅

---
+++ backend/src/main/java/chongchong/WithoutcApplication.java
@@ -0,0 +1,13 @@
+package chongchong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithoutcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithoutcApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/chongchong/WithoutcApplicationTests.java
@@ -0,0 +1,13 @@
+package chongchong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithoutcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
