8f7addc5563f2111222de7e3dec76d70b2ae69eb
Antoliny0919 <antoliny0919@gmail.com>
2026-08-12 00:26:59 +0900

    chore: 테스트 파일을 루트에서 분리

--- frontend/src/test-example/rtl.spec.tsx
+++ frontend/src/test-example/rtl.spec.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import App from './App';
+import App from '../../App';
 
 test('버튼을 클릭하면 카운트가 1 증가한다', async () => {
   render(<App />);
