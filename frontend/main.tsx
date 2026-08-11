bc6757a3c231196315f76b2e2c995caf1f120adc
Antoliny0919 <antoliny0919@gmail.com>
2026-08-11 20:26:18 +0900

    build: React-Router 의존성 추가

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -1,4 +1,19 @@
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router';
 import App from './App';
+import Antoliny from './Antoliny';
 
-createRoot(document.getElementById('root')!).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/antoliny',
+    element: <Antoliny />,
+  },
+]);
+
+const root = document.getElementById('root')!;
+
+ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
